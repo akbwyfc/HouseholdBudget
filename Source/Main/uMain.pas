686bb1f444f96b8e70047d4769169f0eb8bd65ff
akbwyfc <55910280+akbwyfc@users.noreply.github.com>
2026-07-13 21:18:19 +0900

    Create uMain.pas

---
+++ Source/Main/uMain.pas
@@ -0,0 +1,150 @@
+unit uMain;
+
+interface
+
+uses
+  Winapi.Windows,
+  Winapi.Messages,
+
+  System.SysUtils,
+  System.Classes,
+
+  Vcl.Graphics,
+  Vcl.Controls,
+  Vcl.Forms,
+  Vcl.Dialogs,
+  Vcl.StdCtrls,
+  Vcl.ExtCtrls,
+  Vcl.DBGrids,
+  Data.DB,
+  FireDAC.Comp.Client,
+
+  uDatabase,
+  uRepository;
+
+type
+  TfrmMain = class(TForm)
+
+    lblIncome : TLabel;
+    lblExpense : TLabel;
+    lblBalance : TLabel;
+
+    pnlTop : TPanel;
+    pnlButtons : TPanel;
+
+    DBGrid1 : TDBGrid;
+
+    btnAdd : TButton;
+    btnEdit : TButton;
+    btnDelete : TButton;
+    btnCategories : TButton;
+    btnBudget : TButton;
+    btnExit : TButton;
+
+    dsTransactions : TDataSource;
+
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+
+    procedure btnExitClick(Sender: TObject);
+    procedure btnAddClick(Sender: TObject);
+    procedure btnEditClick(Sender: TObject);
+    procedure btnDeleteClick(Sender: TObject);
+    procedure btnCategoriesClick(Sender: TObject);
+    procedure btnBudgetClick(Sender: TObject);
+
+  private
+
+    FDatabase : TDatabase;
+    FRepository : TRepository;
+    FQuery : TFDQuery;
+
+    procedure RefreshDashboard;
+
+  public
+
+  end;
+
+var
+  frmMain : TfrmMain;
+
+implementation
+
+{$R *.dfm}
+
+procedure TfrmMain.FormCreate(Sender: TObject);
+begin
+
+  FDatabase := TDatabase.Create;
+  FDatabase.Connect;
+
+  FRepository := TRepository.Create(FDatabase);
+
+  FQuery := TFDQuery.Create(nil);
+  FQuery.Connection := FDatabase.Connection;
+
+  dsTransactions.DataSet := FQuery;
+
+  RefreshDashboard;
+
+end;
+
+procedure TfrmMain.FormDestroy(Sender: TObject);
+begin
+
+  FQuery.Free;
+  FRepository.Free;
+  FDatabase.Free;
+
+end;
+
+procedure TfrmMain.RefreshDashboard;
+begin
+
+  FRepository.GetTransactions(FQuery);
+
+  lblIncome.Caption :=
+    Format('Income : %.2f',
+      [FRepository.GetMonthlyIncome(YearOf(Date),MonthOf(Date))]);
+
+  lblExpense.Caption :=
+    Format('Expense : %.2f',
+      [FRepository.GetMonthlyExpense(YearOf(Date),MonthOf(Date))]);
+
+  lblBalance.Caption :=
+    Format('Balance : %.2f',
+      [FRepository.GetBalance]);
+
+end;
+
+procedure TfrmMain.btnAddClick(Sender: TObject);
+begin
+  ShowMessage('Not implemented yet.');
+end;
+
+procedure TfrmMain.btnEditClick(Sender: TObject);
+begin
+  ShowMessage('Not implemented yet.');
+end;
+
+procedure TfrmMain.btnDeleteClick(Sender: TObject);
+begin
+  ShowMessage('Not implemented yet.');
+end;
+
+procedure TfrmMain.btnCategoriesClick(Sender: TObject);
+begin
+  ShowMessage('Not implemented yet.');
+end;
+
+procedure TfrmMain.btnBudgetClick(Sender: TObject);
+begin
+  ShowMessage('Not implemented yet.');
+end;
+
+procedure TfrmMain.btnExitClick(Sender: TObject);
+begin
+  Close;
+end;
+
+end.
